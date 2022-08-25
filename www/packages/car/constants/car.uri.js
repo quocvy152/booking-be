effcadf5492696732ce3fafdbec089e56a63370c
quocvy152 <quocvy152@gmail.com>
2022-08-25 19:47:57 +0700

    API Update Carr

--- www/packages/car/constants/car.uri.js
+++ www/packages/car/constants/car.uri.js
@@ -3,7 +3,7 @@ const BASE_ROUTE = '/cars';
 const CF_ROUTINGS_CAR = {
 	CARS: `${BASE_ROUTE}`,
 	REGISTER_CARS: `${BASE_ROUTE}/register`,
-	UPDATE_CARS: `${BASE_ROUTE}/update`,
+	UPDATE_CARS: `${BASE_ROUTE}/update/:carID`,
 	CARS_CARID: `${BASE_ROUTE}/:carID`,
 	GET_LIST_MY_CARS: `${BASE_ROUTE}/list/my-cars`,
 
